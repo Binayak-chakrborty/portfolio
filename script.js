996b090456ed9eb60accf1ea8a14eb5ab7c7ec91
Binayak-chakrborty <138184617+Binayak-chakrborty@users.noreply.github.com>
2024-10-01 22:51:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+ScrollReveal({
+   distance:'80px',
+   duration:1000,
+   delay:100
+});
+
+ScrollReveal().reveal('.contains h1', { origin: 'top' });
+// ScrollReveal().reveal('.img1', { origin: 'right' });
+ScrollReveal().reveal('.btn', { origin: 'bottom' });
+ScrollReveal().reveal('.contains h2', { origin: 'right' });
+ScrollReveal().reveal('#element', { origin: 'right' });
+ScrollReveal().reveal('.contain2', { origin: 'left' });
+ScrollReveal().reveal('.s1', { origin: 'left' });
+ScrollReveal().reveal('.s2', { origin: 'bottom' });
+ScrollReveal().reveal('.s3', { origin: 'bottom' });
+ScrollReveal().reveal('.s4', { origin: 'right' });
+ScrollReveal().reveal('.heading', { origin:'left' });
+ScrollReveal().reveal('.h1', { origin: 'right' });
+ScrollReveal().reveal('.h2', { origin: 'bottom' });
+ScrollReveal().reveal('.h3', { origin: 'left' });
+ScrollReveal().reveal('.box1', { origin: 'right' });
+ScrollReveal().reveal('input', { origin: 'left' });
+ScrollReveal().reveal('textarea', { origin: 'top' });
+// ScrollReveal().reveal('.img1', { origin: 'rigth' });
+// ScrollReveal().reveal('.img1', { origin: 'rigth' });
+// ScrollReveal().reveal('.img1', { origin: 'rigth' });
+// ScrollReveal().reveal('.img1', { origin: 'rigth' });
